cc6ab4b0ec9b94aa6c83ae12a7abced25c0f7d97
damine22 <113331870+damine22@users.noreply.github.com>
2022-09-17 10:55:24 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main{
 	public static void main(String[] args){
 		System.out.println("Hello, World!");
-		System.out.println("this is my first test from Jenkins!");
+		System.out.println("this is my second test from Jenkins!");
 	}
 }
 
