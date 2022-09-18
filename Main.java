51303088a8d53c7b4625392f2e365fe0fc920e5b
damine22 <113331870+damine22@users.noreply.github.com>
2022-09-18 14:15:46 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main{
 	public static void main(String[] args){
 		System.out.println("Hello, Amine!");
-		System.out.println("this is my first trigger test from Jenkins!");
+		System.out.println("this is my first Pipeline test from Jenkins!");
 	}
 }
 
